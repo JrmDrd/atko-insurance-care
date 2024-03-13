89938af9ebe808e54d7d00d6abd361e3019f9f00
Jeremy Durand <durand.jrm@gmail.com>
2024-03-13 16:56:15 +0100

    fix baseURL

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ console.log(process.env.CLIENT_ID);
 
 const port = process.env.PORT || 3001;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
-  config.baseURL = `http://localhost:${port}`;
+  config.baseURL = `13.36.208.72:${port}`;
 }
 
 app.use(auth(config));
